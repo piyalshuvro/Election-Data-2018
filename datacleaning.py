940d8bbd54c5cde99e2663d5593434fc3951d670
Piyal Shuvro <piyalshuvro@gmail.com>
2018-11-20 20:50:43 +1100

    FuzzyMatching
    Added Fuzzy matching

--- datacleaning.py
+++ datacleaning.py
@@ -3,24 +3,75 @@ import numpy as np
 import os
 import glob
 import shutil
+import re
 
-path = "C:\Monash RA Work\Election 2018\Final Data"
-for f in glob.glob(path+"\*.csv"):
+path = "C:\Monash RA Work\Election 2018\Code\ALL DATA Final.csv"
+output_path = "C:\Monash RA Work\Election 2018"
+#
+# for x in os.listdir(path):
+#
+#     print(path+"\\"+x)
+# merge all data
+# all_data = []
+# for f in glob.glob(output_path + "\*.csv"):
+#     all_data.append(pd.read_csv(f))
+#
+# df = pd.concat(all_data, ignore_index=True)
+# df.to_csv(output_path + '\ALL DATA Final.csv', index=False)
 
-    df = pd.read_csv(f)
-    df.columns = ['a', 'b','c','d','e','f','g']
-    df['h'] =  df["a"].apply(lambda x: x if x.isdigit()==False else None)
-    df['i'] = df["a"].apply(lambda x: x if x.isdigit() else None)
-    df = df.fillna(method="ffill")
-    df = df.dropna().reset_index(drop=True)
-    df['j'] = df["a"].apply(lambda x: x if x.isdigit() else None)
-    df = df.dropna().reset_index(drop=True)
-    del df['i']
-    del df['j']
-    df = df[['h','a','b','c','d','e','f','g']]
-    df.columns = ['Block Name','Sl.No','Candidate Name', 'Father/Husband', 'Gender', 'Category', 'Party Affiliation', 'Votes in Favour']
+# 2013 data
+#
+# df = pd.read_csv(path)
+# seatno = []
+# for string in df['seatidentifation1']:
+#
+#     if "/" in string:
+#         new = "/" + string.split("/")[0] + "-" + str(int(string.split("/")[1]))
+#         seatno.append(new)
+#     elif "-" in string:
+#         if "`" in string:
+#             string = string.replace("`","")
+#         new = "/" + string.split("-")[0] + "/-" + str(int(string.split("-")[1]))
+#
+#         seatno.append(new)
+# df['new'] = df['GP'] + seatno
+# df.to_csv("new.csv", index=False)
 
-    df.to_csv(os.path.basename(f),index=False)
+
+#2018 data
+#
+# df = pd.read_csv(path)
+# seatno = []
+# for string in df['Seat Name']:
+#
+#     if "/" in string:
+#         print(string)
+#         new = string.split("/")[0] + "/" + string.split("/")[1].split('-')[0] + "-" + str(int(string.split("/")[1].split('-')[1]))
+#         seatno.append(new)
+#     elif "-" in string:
+#         if "`" in string:
+#             string = string.replace("`","")
+#         new = "/" + string.split("-")[0] + "/-" + str(int(string.split("-")[1]))
+#
+#         seatno.append(new)
+# df['new'] = df['GP'] + seatno
+# df.to_csv("new.csv", index=False)
+
+mapping_df = pd.read_csv("new2013.csv",header=0)
+# print(mapping_df.head())
+# print(list(mapping_df))
+
+data_df = pd.read_csv("ALL DATA Final.csv",header=0)
+# print(list(data_df))
+# print(data_df.head())
+
+# gp_df = pd.read_csv("GP_Code_Details.csv")
+# print(list(gp_df))
+
+df = pd.merge(mapping_df , data_df, how = 'outer',left_on='new',right_on='Seat Name')
+df.to_csv("outer2.csv",index=False)
+
+
 
 
 
